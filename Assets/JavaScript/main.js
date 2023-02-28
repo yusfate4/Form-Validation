a8334673bcb7eb548d936cd85a15891adb330758
Dahud Yusuf <abiodunyusuf4@gmail.com>
2023-02-28 13:28:39 +0100

    form valid with javascript

---
+++ Assets/JavaScript/main.js
@@ -0,0 +1,77 @@
+const form = document.getElementById("form");
+const firstName = document.getElementById("fname");
+const lastName = document.getElementById("lname");
+const phoneNumber = document.getElementById("phone");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  validateInputs();
+});
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
+};
+
+const setSuccess = (element) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = "";
+  inputControl.classList.add("success");
+  inputControl.classList.remove("error");
+};
+
+const isValidEmail = (email) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+const validateInputs = () => {
+  const firstNameValue = firstName.value.trim();
+  const lastNameValue = lastName.value.trim();
+  const phoneNumberValue = phoneNumber.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (firstNameValue === "") {
+    setError(firstName, "this field is required");
+  } else {
+    setSuccess(firstName);
+  }
+
+  if (lastNameValue === "") {
+    setError(lastName, "this field is required");
+  } else {
+    setSuccess(lastName);
+  }
+
+  if (phoneNumberValue === "") {
+    setError(phoneNumber, "this field is required");
+  } else {
+    setSuccess(phoneNumber);
+  }
+  if (emailValue === "") {
+    setError(email, "Email is required");
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, "Provide a valid email address");
+  } else {
+    setSuccess(email);
+  }
+
+  if (passwordValue === "") {
+    setError(password, "Password is required");
+  } else if (passwordValue.length < 8) {
+    setError(password, "Password must be at least 8 character.");
+  } else {
+    setSuccess(password);
+  }
+};
